194194473cec6f367792d80fe8249e903e6e2852
nmcb <marco.borst@splatter.nl>
2016-01-25 17:28:14 +0100

    preparing for play-swagger-0.1.4, wip

--- app/controllers/echo.yaml.scala
+++ app/controllers/echo.yaml.scala
@@ -1,31 +1,33 @@
 
-
-
-
 import play.api.mvc.{Action, Controller}
+
 import play.api.data.validation.Constraint
+
 import de.zalando.play.controllers._
+
 import PlayBodyParsing._
+
 import PlayValidations._
 
-package echo.yaml {
+
 
 import scala.util.Success
 
 
-class EchoYaml extends EchoYamlBase {
+
+package echo.yaml {
+
+    class EchoYaml extends EchoYamlBase {
         val get = getAction {
-            _ => Success(Some(""))
-        } //////// EOF ////////
-        val post = postAction {
-            input: (PostName, PostName) =>
+            _ => Success("")
+        } //////// EOF ////////  getAction
+        val post = postAction { input: (PostName, PostName) =>
             val (name, year) = input
-            Success(Some(PostResponses200Opt(name, year)))
-        } //////// EOF ////////
-        val getTest_pathById = getTest_pathByIdAction {
-            (id: String) =>
-            Success(Some(id))
-        } //////// EOF ////////
-        }
+            Success(PostResponses200(name, year))
+        } //////// EOF ////////  postAction
+        val gettest_pathById = gettest_pathByIdAction { (id: String) =>
+          Success(id)
+        } //////// EOF ////////  gettest_pathByIdAction
+    }
 }
 
